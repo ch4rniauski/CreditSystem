f8e8841f16d38ecf44f1030d089e6de1c3e55bd6
Yauheni Charniauski <chernyavskij.z@mail.ru>
2026-03-29 10:54:55 +0300

    Init frontend

---
+++ CreditSystem/CreditSystem/Controllers/CreditSystemController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreditSystem.Controllers;
+
+[ApiController]
+[Route("credits")]
+public class CreditSystemController : ControllerBase
+{
+    
+}
--- CreditSystem/CreditSystem/Program.cs
+++ CreditSystem/CreditSystem/Program.cs
@@ -4,11 +4,21 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddCors(opt =>
+    opt.AddDefaultPolicy(policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    }));
 
 builder.Services.AddDbContext<CreditSystemContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("CreditSystem")));
 
 var app = builder.Build();
+
+app.UseCors();
 
 app.MapControllers();
 
