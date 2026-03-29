2762715584e80692804d301296fe1f91c656c81d
Yauheni Charniauski <chernyavskij.z@mail.ru>
2026-03-29 10:25:49 +0300

    Move connection string to avoid hardcode

--- CreditSystem/CreditSystem/Program.cs
+++ CreditSystem/CreditSystem/Program.cs
@@ -1,5 +1,15 @@
+using CreditSystem.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<CreditSystemContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("CreditSystem")));
+
 var app = builder.Build();
+
+app.MapControllers();
 
 await app.RunAsync();
